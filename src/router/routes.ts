f7caebedc2c3eaee5430b2cd600fac712fd00b96
772 <thsing772@163.com>
2025-02-21 12:42:35 +0800

    修复github pages不支持history路由导致的刷新404异常

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import demoRoutes from './demoRoutes'
 import IndexView from '@/views/index.vue';
 
+//github pages不支持history模式路由，gitpages部署通过npx cross-env VITE_USE_HASH=1注入变量
+let useHash = import.meta.env.VITE_USE_HASH == 1
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: (useHash ? createWebHashHistory : createWebHistory)(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
@@ -17,4 +19,5 @@ router.beforeEach((to, from, next) => {
   document.title = (to.meta.title as string) ?? 'my demos'
   next()
 })
+
 export default router
